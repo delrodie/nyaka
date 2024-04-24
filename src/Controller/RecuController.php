4c67922a04d03383f4e1e46a0856b0794ad8f610
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2024-04-24 11:30:40 +0000

    Message

--- src/Controller/RecuController.php
+++ src/Controller/RecuController.php
@@ -8,13 +8,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[Route('/recu')]
 class RecuController extends AbstractController
 {
     public function __construct(
         private Gestion $gestion,
-        private AllRepositories $allRepositories
+        private AllRepositories $allRepositories,
+        private HttpClientInterface $httpClient
     )
     {
     }
@@ -22,8 +24,34 @@ class RecuController extends AbstractController
     #[Route('/{matricule}', name: 'app_recu_show', methods: ['GET'])]
     public function show($matricule): Response
     {
+        $aspirant = $this->allRepositories->getOneAspirant(null, $matricule);
+
+        if ($aspirant){
+            dd($this->wave($aspirant));
+        }
         return $this->render('frontend/recu_search.html.twig',[
-            'aspirant' => $this->allRepositories->getOneAspirant(null, $matricule),
+            'aspirant' => $aspirant,
         ]);
+    }
+
+    public function wave($aspirant)
+    {
+        $response = $this->httpClient->request(
+            'GET',
+            "https://api.wave.com/v1/checkout/sessions/{$aspirant->getWaveCheckoutId()}",[
+                'headers' => [
+                    'Authorization' => 'Bearer '.$this->getParameter('wave_api_key'),
+                ],
+            'timeout' => 5
+            ]
+        );
+
+        if ($response->getStatusCode() !== 200){
+            $message =  "HTTP Error ".$response->getStatusCode();
+        }else{
+            $message = $response->toArray();
+        }
+
+        return $message;
     }
 }
